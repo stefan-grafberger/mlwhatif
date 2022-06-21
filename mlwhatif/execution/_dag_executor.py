4c626c6cffc3943345e9cce860b9108ad23324a2
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-06-21 14:26:06 +0200

    First mini dag exec works

---
+++ mlwhatif/execution/_dag_executor.py
@@ -0,0 +1,105 @@
+"""
+The engine to execute the inspections
+"""
+import dataclasses
+
+import networkx
+
+from mlwhatif import DagNode, OperatorType
+
+
+@dataclasses.dataclass(frozen=True)
+class DfResult:
+    """ WIP experiments """
+    node_id: int = dataclasses.field(default=None)
+    dag_node: DagNode = dataclasses.field(default=None)
+    result_df: any = dataclasses.field(hash=False, default=None)
+
+
+class DagExecutor:
+    """ WIP experiments """
+
+    def __init__(self):
+        """ WIP experiments """
+
+    def execute(self, dag: networkx.DiGraph):
+        """ WIP experiments """
+        # pylint: disable=too-many-branches, too-many-statements
+        dag = dag.copy()
+
+        def execute_node(current_node: DagNode):
+            if current_node.operator_info.operator == OperatorType.MISSING_OP:
+                raise Exception("Missing Ops not supported currently!")
+            elif current_node.operator_info.operator == OperatorType.DATA_SOURCE:
+                result_df = current_node.processing_func()
+                result = self.replace_node_with_result(dag, current_node, result_df)
+            elif current_node.operator_info.operator in {OperatorType.SELECTION}:
+                inputs = self.get_required_values(dag, current_node)
+                result_df = current_node.processing_func(*inputs)
+                result = self.replace_node_with_result(dag, current_node, result_df)
+            else:
+                raise NotImplementedError("Unknown op_type: {}".format(type(current_node)))
+            return result
+
+        self.traverse_graph_and_process_nodes(dag, execute_node)
+
+        # TODO: Build mechanism isntead to select what to extract
+        final_result_value = [node for node, out_degree in dag.out_degree() if out_degree == 0][0]
+        return final_result_value.result_df
+
+    @staticmethod
+    def traverse_graph_and_process_nodes(graph: networkx.DiGraph, func):
+        """
+        Traverse the DAG node by node from top to bottom
+        """
+        current_nodes = [node for node in graph.nodes if len(list(graph.predecessors(node))) == 0]
+        processed_nodes = set()
+        while len(current_nodes) != 0:
+            node = current_nodes.pop(0)
+            processed_nodes.add(node.node_id)
+            result_node = func(node)
+            if result_node is not None:
+                children = list(graph.successors(result_node))
+                # Nodes can have multiple parents, only want to process them once we processed all parents
+                for child in children:
+                    if child.node_id not in processed_nodes:
+                        predecessors = [predecessor.node_id for predecessor in graph.predecessors(child)]
+                        if processed_nodes.issuperset(predecessors):
+                            current_nodes.append(child)
+
+        return graph
+
+    @staticmethod
+    def replace_node_with_result(sub_dag, dag_node: DagNode, result_df):
+        """ WIP experiments """
+        new_value_node = DfResult(dag_node.node_id, dag_node, result_df)
+        sub_dag.add_node(new_value_node)
+        for parent_node in sub_dag.predecessors(dag_node):
+            edge_data = sub_dag.get_edge_data(parent_node, dag_node)
+            sub_dag.add_edge(parent_node, new_value_node, **edge_data)
+        for child_node in sub_dag.successors(dag_node):
+            edge_data = sub_dag.get_edge_data(dag_node, child_node)
+            sub_dag.add_edge(new_value_node, child_node, **edge_data)
+        sub_dag.remove_node(dag_node)
+        return new_value_node
+
+    @staticmethod
+    def get_required_values(sub_dag: networkx.DiGraph, current_node: DagNode):
+        """ WIP experiments """
+        required_df_values = []
+        parent_nodes = list(sub_dag.predecessors(current_node))
+        if len(parent_nodes) > 1:
+            parent_nodes_with_arg_index = [(parent_node, sub_dag.get_edge_data(parent_node, current_node))
+                                           for parent_node in parent_nodes]
+            sorted_parent_nodes_with_arg_index = sorted(parent_nodes_with_arg_index, key=lambda x: x[1]['arg_index'])
+            parent_nodes = [node_parent[0] for node_parent in sorted_parent_nodes_with_arg_index]
+
+        for parent_node in parent_nodes:
+            assert isinstance(parent_node, DfResult)
+            df_value = parent_node.result_df
+            sub_dag.remove_edge(parent_node, current_node)
+            # We want to enable garbage collection of value_node if we no longer need to keep the value around
+            if not list(sub_dag.successors(parent_node)):
+                sub_dag.remove_node(parent_node)
+            required_df_values.append(df_value)
+        return required_df_values
---
+++ test/execution/test_pipeline_executor.py
@@ -0,0 +1,24 @@
+from inspect import cleandoc
+
+import pandas as pd
+from mlwhatif.execution._dag_executor import DagExecutor
+from mlwhatif.instrumentation import _pipeline_executor
+
+
+def test_simple_dag_execution():
+    """
+    Tests whether the monkey patching of ('pandas.core.frame', 'dropna') works
+    """
+    test_code = cleandoc("""
+        import pandas as pd
+
+        df = pd.DataFrame([0, 2, 4, 5, None], columns=['A'])
+        assert len(df) == 5
+        df = df.dropna()
+        assert len(df) == 4
+        """)
+    extracted_dag = _pipeline_executor.singleton.run(python_code=test_code, track_code_references=True).dag
+    final_pipeline_result = DagExecutor().execute(extracted_dag)
+
+    df_expected = pd.DataFrame([0, 2, 4, 5.], columns=['A'])
+    pd.testing.assert_frame_equal(final_pipeline_result, df_expected)
