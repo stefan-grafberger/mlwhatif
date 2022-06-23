e6b145ad60e7b07901837789e95865647008296c
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-06-23 19:50:07 +0200

    More todos

--- mlwhatif/execution/_multi_query_optimizer.py
+++ mlwhatif/execution/_multi_query_optimizer.py
@@ -8,9 +8,12 @@ import networkx
 
 class MultiQueryOptimizer:
     """ Combines multiple DAGs and optimizes the joint plan """
+    # pylint: disable=too-few-public-methods
 
     @staticmethod
     def create_optimized_plan(dags: List[networkx.DiGraph]) -> networkx.DiGraph:
         """ Optimize and combine multiple given input DAGs """
         big_execution_dag = networkx.compose_all(dags)
+        # TODO: More optimizations, maybe create some optimization rule interface that what-if analyses can use
+        #  to specify analysis-specific optimizations
         return big_execution_dag
