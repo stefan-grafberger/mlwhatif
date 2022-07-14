32219b272955d7962dfa9cf57f516d13a5e35a00
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-07-14 18:32:00 +0200

    Add todo

--- test/analysis/test_data_corruption.py
+++ test/analysis/test_data_corruption.py
@@ -115,7 +115,56 @@ def test_data_corruption_mini_example_with_projection_modify():
     report = analysis_result.analysis_to_result_reports[data_corruption]
     assert report.shape == (6, 4)
 
-# TODO: Edge-case: train-test-split and no ops modifying columns after
+
+def test_data_corruption_mini_example_only_train_test_split():
+    """
+    Tests whether the Data Corruption analysis works for a very simple pipeline with a DecisionTree score
+    """
+    test_code = cleandoc("""
+        import pandas as pd
+        from sklearn.preprocessing import label_binarize, StandardScaler
+        from sklearn.tree import DecisionTreeClassifier
+        import numpy as np
+
+        df = pd.DataFrame({'A': [0, 0, 0, 0], 'B': [0, 1, 3, 4], 'target': ['no', 'no', 'yes', 'yes']})
+
+        df['A'] = df['A'] / 4
+        df['B'] = df['B'] / 4
+        train = df[['A', 'B']]
+        target = label_binarize(df['target'], classes=['no', 'yes'])
+
+        clf = DecisionTreeClassifier()
+        clf = clf.fit(train, target)
+
+        test_df = pd.DataFrame({'A': [0, 0, 0, 0], 'B':  [4, 3, 4, 3], 'target': ['yes', 'yes', 'yes', 'yes']})
+        test_df['A'] = test_df['A'] / 4
+        test_df['B'] = test_df['B'] / 4
+        test_data = test_df[['A', 'B']]
+        test_labels = label_binarize(test_df['target'], classes=['no', 'yes'])
+        test_score = clf.score(test_data, test_labels)
+        assert test_score == 1.0
+        """)
+
+    def corruption(pandas_df):
+        pandas_df['B'] = 0
+        return pandas_df
+
+    data_corruption = DataCorruption({'A': lambda pandas_df: Scaling(column='A', fraction=1.).transform(pandas_df),
+                                      'B': corruption},
+                                     also_corrupt_train=True)
+
+    analysis_result = PipelineAnalyzer \
+        .on_pipeline_from_string(test_code) \
+        .add_what_if_analysis(data_corruption) \
+        .save_original_dag_to_path(INTERMEDIATE_EXTRACTION_ORIG_PATH) \
+        .save_what_if_dags_to_path(INTERMEDIATE_EXTRACTION_GENERATED_PATH) \
+        .save_optimised_what_if_dags_to_path(INTERMEDIATE_EXTRACTION_OPTIMISED_PATH) \
+        .execute()
+
+    report = analysis_result.analysis_to_result_reports[data_corruption]
+    assert report.shape == (6, 4)
+
+# TODO: Also test manual split using something like date
 
 
 def test_data_corruption_healthcare():
