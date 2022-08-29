b948f9b0e902c3909bb64afe08627b4dd5e7c9b6
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-08-29 17:51:06 +0200

    test_udf_split_and_reuse_worst_case_with_selectivity_safety_active

--- test/optimization/test_udf_split_and_reuse.py
+++ test/optimization/test_udf_split_and_reuse.py
@@ -142,3 +142,133 @@ def test_udf_split_and_reuse_ideal_case(tmpdir):
     analysis_result_without_any_opt.save_what_if_dags_to_path(os.path.join(str(tmpdir), "without-any-opt-what-if"))
     analysis_result_without_any_opt.save_optimised_what_if_dags_to_path(
         os.path.join(str(tmpdir), "without-any-opt-what-if-optimised"))
+
+
+def test_udf_split_and_reuse_worst_case_with_selectivity_safety_active(tmpdir):
+    """
+    Tests whether the .py version of the inspector works
+    """
+    # TODO: use string column, add expensive corruption
+    data_size = 100000
+    variant_count = 2
+
+    df_a_train, df_b_train = get_test_df(int(data_size * 0.8))
+    df_a_path_train = os.path.join(tmpdir, "projection_push_up_df_a_ideal_case_train.csv")
+    df_a_train.to_csv(df_a_path_train, index=False)
+    df_b_path_train = os.path.join(tmpdir, "projection_push_up_df_b_ideal_case_train.csv")
+    df_b_train.to_csv(df_b_path_train, index=False)
+
+    df_a_test, df_b_test = get_test_df(int(data_size * 0.2))
+    df_a_path_test = os.path.join(tmpdir, "projection_push_up_df_a_ideal_case_test.csv")
+    df_a_test.to_csv(df_a_path_test, index=False)
+    df_b_path_test = os.path.join(tmpdir, "projection_push_up_df_b_ideal_case_test.csv")
+    df_b_test.to_csv(df_b_path_test, index=False)
+
+    test_code = cleandoc(f"""
+        import pandas as pd
+        from sklearn.preprocessing import label_binarize, StandardScaler, OneHotEncoder
+        from sklearn.dummy import DummyClassifier
+        from sklearn.pipeline import Pipeline
+        import numpy as np
+        from sklearn.model_selection import train_test_split
+        import fuzzy_pandas as fpd
+        from sklearn.compose import ColumnTransformer
+
+        df_train = pd.read_csv("{df_a_path_train}", engine='python')
+        df_test = pd.read_csv("{df_a_path_test}", engine='python')
+
+        train_target = label_binarize(df_train['target'], classes=['no', 'yes'])
+
+        column_transformer = ColumnTransformer(transformers=[
+                    ('numeric', StandardScaler(), ['A', 'B']),
+                    ('cat', OneHotEncoder(sparse=True, handle_unknown='ignore'), ['group_col_1'])
+                ])
+        pipeline = Pipeline(steps=[
+            ('column_transformer', column_transformer),
+            ('learner', DummyClassifier(strategy='constant', constant=0.))
+        ])
+
+        train_data = df_train[['A', 'B', 'group_col_1']]
+
+        test_target = label_binarize(df_test['target'], classes=['no', 'yes'])
+        test_data = df_test[['A', 'B', 'group_col_1']]
+
+        pipeline = pipeline.fit(train_data, train_target)
+        test_score = pipeline.score(test_data, test_target)
+        assert 0. <= test_score <= 1.
+        """)
+
+    corruption_percentages = []
+    index_filter = []
+    for variant_index in range(variant_count):
+        corruption_percentages.append(variant_index * (0.1 / (variant_count - 1)))
+        index_filter.append(1 + 2 * variant_index)
+
+    data_corruption = WhatIfWrapper(
+        DataCorruption({'A': CorruptionType.BROKEN_CHARACTERS}, also_corrupt_train=True,
+                       corruption_percentages=corruption_percentages),
+        index_filter=index_filter)
+
+    dag_extraction_result = PipelineAnalyzer \
+        .on_pipeline_from_string(test_code) \
+        .execute() \
+        .dag_extraction_info
+
+    analysis_result_with_reuse_opt_rule = PipelineAnalyzer \
+        .on_previously_extracted_pipeline(dag_extraction_result) \
+        .add_what_if_analysis(data_corruption) \
+        .overwrite_optimization_rules([SimpleProjectionPushUp(singleton), UdfSplitAndReuse(singleton)]) \
+        .execute()
+
+    analysis_result_with_pushup_opt_rule = PipelineAnalyzer \
+        .on_previously_extracted_pipeline(dag_extraction_result) \
+        .add_what_if_analysis(data_corruption) \
+        .overwrite_optimization_rules([SimpleProjectionPushUp(singleton)]) \
+        .execute()
+
+    analysis_result_without_opt_rule = PipelineAnalyzer \
+        .on_previously_extracted_pipeline(dag_extraction_result) \
+        .add_what_if_analysis(data_corruption) \
+        .overwrite_optimization_rules([]) \
+        .execute()
+
+    analysis_result_without_any_opt = PipelineAnalyzer \
+        .on_previously_extracted_pipeline(dag_extraction_result) \
+        .add_what_if_analysis(data_corruption) \
+        .skip_multi_query_optimization(True) \
+        .execute()
+
+    assert analysis_result_with_reuse_opt_rule.analysis_to_result_reports[data_corruption].shape == (
+        variant_count + 1, 2)
+    assert analysis_result_with_pushup_opt_rule.analysis_to_result_reports[data_corruption].shape == \
+           (variant_count + 1, 2)
+    assert analysis_result_without_opt_rule.analysis_to_result_reports[data_corruption].shape == (variant_count + 1, 2)
+    assert analysis_result_without_any_opt.analysis_to_result_reports[data_corruption].shape == (variant_count + 1, 2)
+
+    assert analysis_result_with_reuse_opt_rule.runtime_info.what_if_optimized_estimated <= \
+           analysis_result_with_pushup_opt_rule.runtime_info.what_if_optimized_estimated < \
+           analysis_result_without_opt_rule.runtime_info.what_if_optimized_estimated < \
+           analysis_result_without_any_opt.runtime_info.what_if_unoptimized_estimated
+
+    analysis_result_with_reuse_opt_rule.save_original_dag_to_path(os.path.join(str(tmpdir), "with-reuse-opt-orig"))
+    analysis_result_with_reuse_opt_rule.save_what_if_dags_to_path(os.path.join(str(tmpdir), "with-reuse-opt-what-if"))
+    analysis_result_with_reuse_opt_rule.save_optimised_what_if_dags_to_path(os.path.join(str(tmpdir),
+                                                                                         "with-reuse-opt-what-if-"
+                                                                                         "optimised"))
+
+    analysis_result_with_pushup_opt_rule.save_original_dag_to_path(os.path.join(str(tmpdir), "with-pushup-opt-orig"))
+    analysis_result_with_pushup_opt_rule.save_what_if_dags_to_path(
+        os.path.join(str(tmpdir), "with-pushup-opt-what-if"))
+    analysis_result_with_pushup_opt_rule.save_optimised_what_if_dags_to_path(os.path.join(str(tmpdir),
+                                                                                          "with-pushup-opt-what-if"
+                                                                                          "-optimised"))
+
+    analysis_result_without_opt_rule.save_original_dag_to_path(os.path.join(str(tmpdir), "without-opt-orig"))
+    analysis_result_without_opt_rule.save_what_if_dags_to_path(os.path.join(str(tmpdir), "without-opt-what-if"))
+    analysis_result_without_opt_rule.save_optimised_what_if_dags_to_path(
+        os.path.join(str(tmpdir), "without-opt-what-if-optimised"))
+
+    analysis_result_without_any_opt.save_original_dag_to_path(os.path.join(str(tmpdir), "without-any-opt-orig"))
+    analysis_result_without_any_opt.save_what_if_dags_to_path(os.path.join(str(tmpdir), "without-any-opt-what-if"))
+    analysis_result_without_any_opt.save_optimised_what_if_dags_to_path(
+        os.path.join(str(tmpdir), "without-any-opt-what-if-optimised"))
