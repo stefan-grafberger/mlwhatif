a449786c76410b3c0f8b4fe91af27ce2ad01bcef
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-08-31 17:24:28 +0200

    test_filter_push_up_worst_case_original_pipeline

--- test/optimization/test_simple_filter_addition_push_up.py
+++ test/optimization/test_simple_filter_addition_push_up.py
@@ -347,3 +347,110 @@ def test_filter_push_up_worst_case_no_original_pipeline(tmpdir):
     analysis_result_without_any_opt.save_what_if_dags_to_path(os.path.join(str(tmpdir), "without-any-opt-what-if"))
     analysis_result_without_any_opt.save_optimised_what_if_dags_to_path(
         os.path.join(str(tmpdir), "without-any-opt-what-if-optimised"))
+
+
+def test_filter_push_up_worst_case_original_pipeline(tmpdir):
+    """
+    Tests whether the .py version of the inspector works
+    """
+    data_size = 5000
+    variant_count = 3
+
+    df_a_train, df_b_train = get_test_df(int(data_size * 0.8))
+    df_a_path_train = os.path.join(tmpdir, "filter_push_up_df_a_worst_case_no_original_train.csv")
+    df_a_train.to_csv(df_a_path_train, index=False)
+    df_b_path_train = os.path.join(tmpdir, "filter_push_up_df_b_worst_case_no_original_train.csv")
+    df_b_train.to_csv(df_b_path_train, index=False)
+
+    df_a_test, df_b_test = get_test_df(int(data_size * 0.2))
+    df_a_path_test = os.path.join(tmpdir, "filter_push_up_df_a_worst_case_no_original_test.csv")
+    df_a_test.to_csv(df_a_path_test, index=False)
+    df_b_path_test = os.path.join(tmpdir, "filter_push_up_df_b_worst_case_no_original_test.csv")
+    df_b_test.to_csv(df_b_path_test, index=False)
+
+    test_code = cleandoc(f"""
+        import pandas as pd
+        from sklearn.preprocessing import label_binarize, StandardScaler
+        from sklearn.dummy import DummyClassifier
+        import numpy as np
+        from sklearn.model_selection import train_test_split
+        import fuzzy_pandas as fpd
+
+        df_a_train = pd.read_csv("{df_a_path_train}", engine='python')
+        df_a_train = df_a_train[df_a_train['A'] >= 95]
+        df_b_train = pd.read_csv("{df_b_path_train}", engine='python')
+        df_train = fpd.fuzzy_merge(df_a_train, df_b_train, on='str_id', method='levenshtein', keep_right=['C', 'D'],
+            threshold=0.99)
+
+        df_a_test = pd.read_csv("{df_a_path_test}", engine='python')
+        df_a_test = df_a_test[df_a_test['A'] >= 95]
+        df_b_test = pd.read_csv("{df_b_path_test}", engine='python')
+        df_test = fpd.fuzzy_merge(df_a_test, df_b_test, on='str_id', method='levenshtein', keep_right=['C', 'D'],
+            threshold=0.99)
+
+        train_target = df_train['target_featurized']
+        train_data = df_train[['A', 'B']]
+
+        test_target = df_test['target_featurized']
+        test_data = df_test[['A', 'B']]
+
+        clf = DummyClassifier(strategy='constant', constant=0.)
+        clf = clf.fit(train_data, train_target)
+        test_score = clf.score(test_data, test_target)
+        assert 0. <= test_score <= 1.
+        """)
+
+    filter_variants = {}
+    for variant_index in range(variant_count - 1):
+        def filter_func(df, bound_variant_index):
+            return df[df['B'] <= 3]
+
+        filter_variants[f"filter_{variant_index}"] = ("B", partial(filter_func, bound_variant_index=variant_index))
+
+    data_corruption = DataFilterVariants(filter_variants, True)
+
+    dag_extraction_result = PipelineAnalyzer \
+        .on_pipeline_from_string(test_code) \
+        .execute() \
+        .dag_extraction_info
+
+    analysis_result_with_opt_rule = PipelineAnalyzer \
+        .on_previously_extracted_pipeline(dag_extraction_result) \
+        .add_what_if_analysis(data_corruption) \
+        .overwrite_optimization_rules([SimpleFilterAdditionPushUp(singleton)]) \
+        .execute()
+
+    analysis_result_without_opt_rule = PipelineAnalyzer \
+        .on_previously_extracted_pipeline(dag_extraction_result) \
+        .add_what_if_analysis(data_corruption) \
+        .overwrite_optimization_rules([]) \
+        .execute()
+
+    analysis_result_without_any_opt = PipelineAnalyzer \
+        .on_previously_extracted_pipeline(dag_extraction_result) \
+        .add_what_if_analysis(data_corruption) \
+        .skip_multi_query_optimization(True) \
+        .execute()
+
+    assert analysis_result_with_opt_rule.analysis_to_result_reports[data_corruption].shape == (variant_count + 1, 3)
+    assert analysis_result_without_opt_rule.analysis_to_result_reports[data_corruption].shape == (variant_count + 1, 3)
+    assert analysis_result_without_any_opt.analysis_to_result_reports[data_corruption].shape == (variant_count + 1, 3)
+
+    assert analysis_result_with_opt_rule.runtime_info.what_if_optimized_estimated <= \
+           analysis_result_without_opt_rule.runtime_info.what_if_optimized_estimated < \
+           analysis_result_without_any_opt.runtime_info.what_if_unoptimized_estimated
+
+    analysis_result_with_opt_rule.save_original_dag_to_path(os.path.join(str(tmpdir), "with-opt-orig"))
+    analysis_result_with_opt_rule.save_what_if_dags_to_path(os.path.join(str(tmpdir), "with-opt-what-if"))
+    analysis_result_with_opt_rule.save_optimised_what_if_dags_to_path(
+        os.path.join(str(tmpdir), "with-opt-what-if-optimised"))
+
+    analysis_result_without_opt_rule.save_original_dag_to_path(os.path.join(str(tmpdir), "without-opt-orig"))
+    analysis_result_without_opt_rule.save_what_if_dags_to_path(os.path.join(str(tmpdir), "without-opt-what-if"))
+    analysis_result_without_opt_rule.save_optimised_what_if_dags_to_path(
+        os.path.join(str(tmpdir), "without-opt-what-if-optimised"))
+
+    analysis_result_without_any_opt.save_original_dag_to_path(os.path.join(str(tmpdir), "without-any-opt-orig"))
+    analysis_result_without_any_opt.save_what_if_dags_to_path(os.path.join(str(tmpdir), "without-any-opt-what-if"))
+    analysis_result_without_any_opt.save_optimised_what_if_dags_to_path(
+        os.path.join(str(tmpdir), "without-any-opt-what-if-optimised"))
