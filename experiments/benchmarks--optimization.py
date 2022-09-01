0086781e4a349bd9ebd2f0fe353045927d8e799d
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-09-01 15:01:06 +0200

    Producing a proper output now

--- experiments/benchmarks--optimization.py
+++ experiments/benchmarks--optimization.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import tempfile
 import warnings
+
+import pandas
 
 from example_pipelines import HEALTHCARE_PY
 from example_pipelines.healthcare import custom_monkeypatching
@@ -9,7 +12,7 @@ from experiments.operator_deletion_filter_push_up import run_operator_deletion_f
 from experiments.simple_filter_addition_push_up import run_filter_addition_push_up_benchmark
 from experiments.simple_projection_push_up import run_projection_push_up_benchmark
 from experiments.udf_split_and_reuse import run_udf_split_and_reuse_benchmark
-from mlwhatif import PipelineAnalyzer
+from mlwhatif import PipelineAnalyzer, AnalysisResults
 
 warnings.filterwarnings("ignore")
 
@@ -20,7 +23,6 @@ optimization = sys.argv[1]
 scenario = sys.argv[2]
 variant_count = int(sys.argv[3])
 data_size = int(sys.argv[4])
-
 
 cmd_args = []
 if len(sys.argv) > 5:
@@ -44,13 +46,17 @@ _ = PipelineAnalyzer \
     .add_custom_monkey_patching_modules([custom_monkeypatching]) \
     .execute()
 
-
 current_directory = os.getcwd()
 output_directory = os.path.join(current_directory, r'optimization-benchmark-results')
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-
+result_df_repetitions = []
+result_df_optimizations = []
+result_df_scenarios = []
+result_df_variant_counts = []
+result_df_data_sizes = []
+result_df_metrics = {}
 for repetition in range(num_repetitions):
     print(f'# Optimization benchmark for {optimization} with args [{sys.argv[2:]}] '
           f'-- repetition {repetition + 1} of {num_repetitions}')
@@ -58,6 +64,31 @@ for repetition in range(num_repetitions):
         print(f"Valid optimization types: {optimizations_benchmark_funcs.keys()}")
         raise ValueError(f"Optimization type {optimization} is not one of them!")
     benchmark_func = optimizations_benchmark_funcs[optimization]
-    results = benchmark_func(scenario, variant_count, data_size, output_directory)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        results_dict_items = benchmark_func(scenario, variant_count, data_size, tmp_dir).items()
+    results_dict_items = [(result_key, result_value.runtime_info.what_if_execution)
+                          for (result_key, result_value) in results_dict_items
+                          if isinstance(result_value, AnalysisResults)]
+    for result_key, result_value in results_dict_items:
+        metric_value_list = result_df_metrics.get(result_key, [])
+        metric_value_list.append(result_value)
+        result_df_metrics[result_key] = metric_value_list
+    result_df_repetitions.append(repetition)
+    result_df_optimizations.append(optimization)
+    result_df_scenarios.append(scenario)
+    result_df_variant_counts.append(variant_count)
+    result_df_data_sizes.append(data_size)
+
     print(f'# Final results -- repetition {repetition + 1} of {num_repetitions} ')
-    print(results)
+    print(results_dict_items)
+
+result_df = pandas.DataFrame({'repetition': result_df_repetitions,
+                              'optimization': result_df_optimizations,
+                              'scenario': result_df_scenarios,
+                              'variant_counts': result_df_variant_counts,
+                              'data_size': result_df_data_sizes,
+                              **result_df_metrics})
+result_df_path = os.path.join(output_directory, f"results-{optimization}-{scenario}-{variant_count}-{data_size}.csv")
+result_df.to_csv(result_df_path, index=False)
+print(f'# Final results after all repeititions')
+print(result_df)
