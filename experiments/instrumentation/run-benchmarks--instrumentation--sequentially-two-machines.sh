a29c268d2b805dc1e1c327957e1602a5bdcbef91
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-09-23 09:56:31 +0200

    Enable taskset again for instrum benchmark

--- experiments/instrumentation/run-benchmarks--instrumentation--sequentially-two-machines.sh
+++ experiments/instrumentation/run-benchmarks--instrumentation--sequentially-two-machines.sh
@@ -23,8 +23,8 @@ echo "Cores to use: $core_num";
 for data_loading in "${data_loading_options[@]}"
 do
   echo "PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c $core_num python3.9 benchmarks_end_to_end.py $dataset $data_loading image image"
-#  PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c "$core_num" python3.9 benchmarks_instrumentation.py sneakers "$data_loading" image image
-  PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python3.9 benchmarks_instrumentation.py sneakers "$data_loading" image image
+  PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c "$core_num" python3.9 benchmarks_instrumentation.py sneakers "$data_loading" image image
+#  PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python3.9 benchmarks_instrumentation.py sneakers "$data_loading" image image
 done
 
 for dataset in "healthcare" "folktables" "cardio" "reviews"
