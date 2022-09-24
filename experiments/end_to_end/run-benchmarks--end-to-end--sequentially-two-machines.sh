cdc62eea6f5d07839c05da329c0522f10efc1e3b
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-09-24 10:55:40 +0200

    Prepare sequential experiment run

--- experiments/end_to_end/run-benchmarks--end-to-end--sequentially-two-machines.sh
+++ experiments/end_to_end/run-benchmarks--end-to-end--sequentially-two-machines.sh
@@ -19,7 +19,7 @@ fi
 core_num="0-7"
 echo "Cores to use: $core_num";
 
-for scenario in "data_corruption" "data_cleaning"
+for scenario in "data_cleaning" "data_corruption"
 do
   for data_loading in "${data_loading_options[@]}"
   do
@@ -28,15 +28,15 @@ do
   done
 done
 
-for scenario in "feature_importance" "operator_impact" "data_corruption" "data_cleaning"
+for model in "logistic_regression" "xgboost" "neural_network"
 do
-  for dataset in "healthcare" "folktables" "cardio" "reviews"
+  for scenario in "data_cleaning" "data_corruption" "feature_importance" "operator_impact"
   do
-    for data_loading in
+    for dataset in "healthcare" "folktables" "cardio" "reviews"
     do
-      for featurization in "featurization_0" "featurization_1" "featurization_2" "featurization_3" "featurization_4"
+      for data_loading in "${data_loading_options[@]}"
       do
-        for model in "logistic_regression" "xgboost" "neural_network"
+        for featurization in "featurization_0" "featurization_1" "featurization_2" "featurization_3" "featurization_4"
         do
           echo "PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c $core_num python3.9 benchmarks_end_to_end.py $scenario $dataset $data_loading $featurization $model"
           PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c "$core_num" python3.9 benchmarks_end_to_end.py "$scenario" "$dataset" "$data_loading" "$featurization" "$model"
