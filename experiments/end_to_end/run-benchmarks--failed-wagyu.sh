617e6b44e7b18bfcc6b4e9b28b23723b04052507
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-09-25 19:31:47 +0200

    Script for failed runs wagyu

--- experiments/end_to_end/run-benchmarks--failed-wagyu.sh
+++ experiments/end_to_end/run-benchmarks--failed-wagyu.sh
@@ -1,3 +1,3 @@
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c 32-39 python3.9 benchmarks_end_to_end.py data_corruption folktables slow_loading featurization_1 logistic_regression && \
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c 32-39 python3.9 benchmarks_end_to_end.py data_corruption cardio slow_loading featurization_0 xgboost && \
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c 32-39 python3.9 benchmarks_end_to_end.py data_cleaning cardio slow_loading featurization_0 logistic_regression && \
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python taskset -c 32-39 python3.9 benchmarks_end_to_end.py data_cleaning cardio slow_loading featurization_0 logistic_regression
